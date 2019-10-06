a9ef8d663bb0a9de53e13c2ed5efe6965335d809
Igor Artemov <i_artemov@omobus.net>
2019-10-06 10:21:55 +0300

    locked [file_fdw] extension.

--- omobus-proxy-db.sql
+++ omobus-proxy-db.sql
@@ -1,11 +1,11 @@
 /* Copyright (c) 2006 - 2019 omobus-proxy-db authors, see the included COPYRIGHT file. */
 
 create extension hstore;
-create extension file_fdw;
+/*create extension file_fdw;*/
 create extension isn;
 create extension "uuid-ossp";
 
-create server fs foreign data wrapper file_fdw;
+/*create server fs foreign data wrapper file_fdw;*/
 
 
 create sequence seq_accounts;
