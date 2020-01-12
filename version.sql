0844a8ef74e1838c6b18d2217552af2bc56f83b5
Igor Artemov <i_artemov@omobus.net>
2020-01-12 15:41:27 +0300

    3.4.64 release

--- version.sql
+++ version.sql
@@ -1,3 +1,3 @@
 /* Copyright (c) 2006 - 2020 omobus-proxy-db authors, see the included COPYRIGHT file. */
 
-insert into sysparams values('db:vstamp', '3.4.63', 'Database version number.');
+insert into sysparams values('db:vstamp', '3.4.64', 'Database version number.');
