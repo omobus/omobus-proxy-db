6f460c94e00ea34a6ac9e8a307b9aacf2c30dd45
Igor Artemov <i_artemov@omobus.net>
2020-05-03 13:32:36 +0300

    console: fixed [req_canceling] procedures.

--- req_canceling.sql
+++ req_canceling.sql
@@ -1,11 +1,11 @@
 /* Copyright (c) 2006 - 2020 omobus-proxy-db authors, see the included COPYRIGHT file. */
 
-create or replace function console.req_canceling(rlogin uid_t, /*attrs:*/ u_id uid_t, t_id uid_t, b date_t, e date_t, n note_t) returns int
+create or replace function console.req_canceling(rlogin uid_t, _reqdt datetime_t, /*attrs:*/ u_id uid_t, t_id uid_t, b date_t, e date_t, n note_t) returns int
 as $BODY$
 declare
     stack text; fcesig text;
+    hs hstore;
     r date_t;
-    auth descr_t;
 begin
     GET DIAGNOSTICS stack = PG_CONTEXT;
     fcesig := substring(stack from 'function console\.(.*?)\(');
@@ -31,26 +31,40 @@ begin
 	perform content_add('route_compliance', '', r, r);
     end loop;
 
-    select descr from users where user_id=rlogin
-	into auth;
+    perform evmail_add(
+	u_id, 
+	'canceling/caption:new', 
+	'canceling/body:new', 
+	3::smallint /*normal*/, 
+	array[
+	    'doc_note', NIL(trim(n)),
+	    'b_date', "L"(b),
+	    'e_date', "L"(e),
+	    'canceling_type', (select lower(descr) from canceling_types where canceling_type_id = t_id),
+	    'u_name', coalesce((select descr from users where user_id = rlogin),lower(rlogin))
+	]
+    );
 
-    perform evmail_add(u_id, 'canceling/caption:new', 'canceling/body:new', 3::smallint /*normal*/, array[
-	'doc_note',NIL(trim(n)),'b_date',"L"(b),'e_date',"L"(e),
-	'canceling_type',(select lower(descr) from canceling_types where canceling_type_id = t_id),
-	'u_name',case when auth is null then lower(rlogin) else auth end]);
+    hs := hstore(array['user_id',u_id]);
+    hs := hs || hstore(array['b_date',b]);
+    hs := hs || hstore(array['e_date',e]);
+    hs := hs || hstore(array['canceling_type_id',t_id]);
+    if( n is not null ) then
+	hs := hs || hstore(array['doc_note',n]);
+    end if;
 
-    insert into console.requests(req_login, req_type, status, attrs)
-	values(rlogin, fcesig, 'set', hstore(array['user_id',u_id,'b_date',b,'e_date',e,'canceling_type_id',t_id,'doc_note',n]));
+    insert into console.requests(req_login, req_type, req_dt, status, attrs)
+	values(rlogin, fcesig, _reqdt, 'set', hs);
 
     return 0;
 end;
 $BODY$ language plpgsql;
 
-create or replace function console.req_canceling(rlogin uid_t, cmd code_t, /*attrs:*/ u_id uid_t, p_date date_t) returns int
+create or replace function console.req_canceling(rlogin uid_t, _reqdt datetime_t, cmd code_t, /*attrs:*/ u_id uid_t, p_date date_t) returns int
 as $BODY$
 declare
     stack text; fcesig text;
-    auth descr_t;
+    hs hstore;
 begin
     GET DIAGNOSTICS stack = PG_CONTEXT;
     fcesig := substring(stack from 'function console\.(.*?)\(');
@@ -72,14 +86,22 @@ begin
     perform content_add('tech_route', u_id, p_date, p_date);
     perform content_add('route_compliance', '', p_date, p_date);
 
-    select descr from users where user_id=rlogin
-	into auth;
+    perform evmail_add(
+	u_id, 
+	'canceling/caption:'||cmd, 
+	'canceling/body:'||cmd, 
+	case when cmd = 'revoke' then 2::smallint /*high*/ else 3::smallint /*normal*/ end, 
+	array[
+	    'route_date', "L"(p_date), 
+	    'u_name', coalesce((select descr from users where user_id = rlogin),lower(rlogin))
+	]
+    );
 
-    perform evmail_add(u_id, 'canceling/caption:'||cmd, 'canceling/body:'||cmd, case when cmd = 'revoke' then 2::smallint /*high*/ else 3::smallint /*normal*/ end, array[
-	'route_date',"L"(p_date), 'u_name',case when auth is null then lower(rlogin) else auth end]);
+    hs := hstore(array['user_id',u_id]);
+    hs := hs || hstore(array['route_date',p_date]);
 
-    insert into console.requests(req_login, req_type, status, attrs)
-	values(rlogin, fcesig, cmd, hstore(array['user_id',u_id,'route_date',p_date]));
+    insert into console.requests(req_login, req_type, req_dt, status, attrs)
+	values(rlogin, fcesig, _reqdt, cmd, hs);
 
     return 0;
 end;
