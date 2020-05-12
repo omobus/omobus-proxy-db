d0dfa2c6016477de1e3d94cd8b926a6a6723af56
Igor Artemov <i_artemov@omobus.net>
2020-05-12 18:50:38 +0300

    docs, console: added notification to the [addtion*] document and console procedure.

--- req_addition.sql
+++ req_addition.sql
@@ -5,6 +5,13 @@ create or replace function console.req_addition(_login uid_t, _reqdt datetime_t,
 as $BODY$
 declare
     stack text; fcesig text;
+    msg_body varchar(4096);
+    msg_cap varchar(512);
+    rcpt_to emails_t;
+    u_lang lang_t;
+    auth descr_t;
+    a_name descr_t;
+    a_address address_t;
     updated_rows int = 0;
     g uid_t;
 begin
@@ -17,7 +24,9 @@ begin
 
     if( _cmd = 'validate' ) then
 	update j_additions set validator_id = _login, validated = 1, updated_ts = current_timestamp
-	    where doc_id = _docid and hidden = 0 and validated = 0;
+	    where doc_id = _docid and hidden = 0 and validated = 0
+	returning guid
+	into g;
 	GET DIAGNOSTICS updated_rows = ROW_COUNT;
     elsif( _cmd = 'reject' ) then
 	update j_additions set hidden = 1, updated_ts = current_timestamp
@@ -30,6 +39,27 @@ begin
 	    where account_id = g and approved = 0 and hidden = 0;
     else
 	raise exception '% doesn''t support [%] command! Accepted commands are [validate|reject].', fcesig, _cmd;
+    end if;
+
+    if updated_rows > 0 then
+	select descr from users where user_id = _login
+	    into auth;
+	select descr, address from accounts where account_id = g
+	    into a_name, a_address;
+
+	for rcpt_to, u_lang in select evaddrs, lang_id from users where user_id in (
+		select user_id from j_additions where doc_id = _docid
+	    ) and hidden=0 and evaddrs is not null and array_length(evaddrs, 1) > 0
+	loop
+	    msg_cap  := "L10n_format_a"(u_lang, 'evmail', '', 'addition/caption:'||_cmd, array['a_name',a_name]);
+	    msg_body := "L10n_format_a"(u_lang, 'evmail', '', 'addition/body:'||_cmd, array['a_name',a_name,'address',a_address,
+		'u_name',case when auth is null then lower(_login) else auth end]);
+
+	    if( msg_cap is not null and msg_body is not null ) then
+		insert into mail_stream (rcpt_to, cap, msg, content, priority)
+		    values (rcpt_to, msg_cap, msg_body, 'text/html', 3 /*normal*/);
+	    end if;
+	end loop;
     end if;
 
     insert into console.requests(req_login, req_type, req_dt, status, attrs)
