021f782a40ad7405d696da32ab8b6fc7175f10e1
Igor Artemov <i_artemov@omobus.net>
2020-06-07 12:13:07 +0300

    cids: deleted service.

--- omobus-proxy-db.sql
+++ omobus-proxy-db.sql
@@ -824,19 +824,6 @@ create table symlinks (
 create index i_db_ids_symlinks on symlinks using GIN (db_ids);
 create trigger trig_updated_ts before update on symlinks for each row execute procedure tf_updated_ts();
 
-create table syscellids (
-    source 		code_t 		not null,
-    mcc 		int32_t 	not null, /* mobile country code */
-    mnc 		int32_t 	not null, /* mobile network code */
-    cid 		int32_t 	not null, /* cell tower ID */
-    lac 		int32_t 	not null, /* local area code */
-    latitude 		gps_t 		not null,
-    longitude 		gps_t 		not null,
-    radio 		text 		not null,
-    changeable 		bool_t 		not null, /* defines if coordinates of the cell tower are exact (0) or approximate (1). */
-    primary key(source, mcc, mnc, cid, lac)
-);
-
 create table sysholidays (
     h_date 		date_t 		not null,
     country_id 		country_t 	not null,
