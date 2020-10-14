15dcd0e1d4abdaa78c2e0eb5901f27dfac08c085
Igor Artemov <i_aetemov@omobus.net>
2020-10-14 13:53:28 +0300

    3.5.10 release

--- version.sql
+++ version.sql
@@ -1,3 +1,3 @@
 /* Copyright (c) 2006 - 2020 omobus-proxy-db authors, see the included COPYRIGHT file. */
 
-insert into sysparams values('db:vstamp', '3.5.9', 'Database version number.');
+insert into sysparams values('db:vstamp', '3.5.10', 'Database version number.');
