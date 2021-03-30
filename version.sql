00756a12b6a40ab3af6c2516204f90bea62b371f
Igor Artemov ( <i_artemov@omobus.net>
2021-03-30 17:54:23 +0300

    3.5.17 release

--- version.sql
+++ version.sql
@@ -1,3 +1,3 @@
 /* Copyright (c) 2006 - 2021 omobus-proxy-db authors, see the included COPYRIGHT file. */
 
-insert into sysparams values('db:vstamp', '3.5.16', 'Database version number.');
+insert into sysparams values('db:vstamp', '3.5.17', 'Database version number.');
